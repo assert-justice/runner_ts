5f01a44d979fadc8ffd07ad53a8debf2bdafb8d9
riley <rileywestonmiller@outlook.com>
2023-06-27 05:46:15 -0700

    add entity

---
+++ src/entity.ts
@@ -0,0 +1,11 @@
+import { Vector2 } from "./la";
+import { Sprite } from "./sprite";
+
+export class Entity{
+    position: Vector2;
+    constructor(){
+        this.position = new Vector2();
+    }
+    update(_: number){}
+    draw(){}    
+}
